6412e11f909aae54350d74450463986829aab518
thesdony <neothesdony@pm.me>
2026-09-12 15:13:16 +0700

    chore: bump version to 0.1.0-beta-5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.neop2p.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.0-beta-4"
+        versionCode = 6
+        versionName = "0.1.0-beta-5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
