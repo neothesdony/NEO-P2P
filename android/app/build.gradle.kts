9a987bbbbd9a26bbc36935e80ec7d9d7bb8d67eb
thesdony <neothesdony@pm.me>
2026-09-13 19:59:38 +0700

    chore(release): v0.1.0-beta-6 mainnet build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.neop2p.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.0-beta-5"
+        versionCode = 7
+        versionName = "0.1.0-beta-6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -72,7 +72,7 @@ android {
     }
 
     defaultConfig {
-        buildConfigField("String", "NETWORK", "\"testnet\"")
+        buildConfigField("String", "NETWORK", "\"mainnet\"")
     }
 
     testOptions {
