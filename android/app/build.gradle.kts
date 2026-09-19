4b97374bdc41a013f35e8b1a6f4761c8864e31c0
thesdony <neothesdony@pm.me>
2026-09-19 16:19:16 +0700

    chore(release): v0.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.neop2p.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.1.0-beta-8"
+        versionCode = 10
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
